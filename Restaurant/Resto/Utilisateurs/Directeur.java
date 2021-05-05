0fa2cbd2f589a241e759c28c2984f6fa1a214804
ColleenCallonego <colleencallonego@gmail.com>
2021-05-05 17:19:35 +0200

    US#21 finie

--- Restaurant/Resto/Utilisateurs/Directeur.java
+++ Restaurant/Resto/Utilisateurs/Directeur.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Directeur extends Utilisateur{
+public class Directeur extends Utilisateur {
 
-    public Directeur(String id){
+    public Directeur(String id) {
         super(id);
     }
 
@@ -29,7 +29,7 @@ public class Directeur extends Utilisateur{
                 + "0. Se déconnecter" + n + n + n + "Que voulez vous faire?");
         try {
             rep = scan.nextInt();
-            if (!verifDirPrinc(rep)) {
+            if (!verif(rep, 8)) {
                 System.out.println("Entrée non valide");
             }
         } catch (InputMismatchException e) {
@@ -39,24 +39,4 @@ public class Directeur extends Utilisateur{
         scan.close();
         return rep;
     }
-
-    /**
-     * Méthode pour vérifier que le choix de directeur est possible
-     *
-     * @param entree choix du directeur
-     * @return true si le choix existe et false sinon
-     */
-    public static Boolean verifDirPrinc(int entree) {
-        ArrayList<Integer> possibilite = new ArrayList<Integer>();
-        possibilite.add(0);
-        possibilite.add(1);
-        possibilite.add(2);
-        possibilite.add(3);
-        possibilite.add(4);
-        possibilite.add(5);
-        possibilite.add(6);
-        possibilite.add(7);
-        return possibilite.contains(entree);
-    }
-
 }
--- Restaurant/Resto/Utilisateurs/Utilisateur.java
+++ Restaurant/Resto/Utilisateurs/Utilisateur.java
@@ -1,13 +1,22 @@
 package Restaurant.Resto.Utilisateurs;
 
+import java.util.ArrayList;
+
 public abstract class Utilisateur {
     protected String id;
 
-    public Utilisateur(String id){
+    public Utilisateur(String id) {
         this.id = id;
     }
 
     public abstract Integer afficherPrincipal();
 
+    public static Boolean verif(int entree, int nbChoix) {
+        ArrayList<Integer> possibilite = new ArrayList<Integer>();
+        for (int i = 0; i < nbChoix; i++) {
+            possibilite.add(i);
+        }
+        return possibilite.contains(entree);
+    }
 
 }
