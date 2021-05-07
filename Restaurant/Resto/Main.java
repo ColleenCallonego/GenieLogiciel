ad69b9042f4389437ac3429e1f9d99f9370c0758
ColleenCallonego <colleencallonego@gmail.com>
2021-05-07 22:03:22 +0200

    US#24 Sans Test

--- Restaurant/Resto/Main.java
+++ Restaurant/Resto/Main.java
@@ -1,8 +1,11 @@
 package Restaurant.Resto;
 
+import Restaurant.Resto.Utilisateurs.Cuisinier;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Cuisinier c = new Cuisinier("1");
+        c.definitionPlat();
     }
 }
