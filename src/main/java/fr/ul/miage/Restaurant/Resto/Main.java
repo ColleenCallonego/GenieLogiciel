e5da644efcfec4678902fb21e02790a691dd3445
ColleenCallonego <colleencallonego@gmail.com>
2021-05-08 18:20:51 +0200

    US#24 Tests

--- src/main/java/fr/ul/miage/Restaurant/Resto/Main.java
+++ src/main/java/fr/ul/miage/Restaurant/Resto/Main.java
@@ -1,11 +1,10 @@
 package fr.ul.miage.Restaurant.Resto;
 
-import fr.ul.miage.Restaurant.Resto.Utilisateurs.Cuisinier;
+import fr.ul.miage.Restaurant.Resto.Utilisateurs.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Cuisinier c = new Cuisinier("1");
-        c.definitionPlat();
+         
     }
 }
