b4e8004901600e2d7b305951572ea16168f528b8
savaleinart <savaleinart@gmail.com>
2021-05-15 18:08:53 +0100

    creation d'une classe pour les operations sur la BDD

---
+++ src/main/java/fr/ul/miage/Restaurant/Resto/misc/GestionBDD.java
@@ -0,0 +1,40 @@
+package fr.ul.miage.Restaurant.Resto.misc;
+
+import java.sql.*;
+
+public class GestionBDD {
+    public static Connection connect() {
+        String url = "jdbc:postgresql://plg-broker.ad.univ-lorraine.fr/Restaurant_G8";
+        String user = "m1user1_03";
+        String password = "m1user1_03";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public  static ResultSet executeSelect(Connection conn, String query){
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void executeUpdate(Connection conn, String query){
+        try {
+            Statement st = conn.createStatement();
+            st.executeUpdate(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
