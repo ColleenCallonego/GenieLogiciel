fc9d63959c29b644f5890c0b641f0824cb46a7ec
ColleenCallonego <colleencallonego@gmail.com>
2021-05-23 22:16:10 +0200

    US#11

--- src/main/java/fr/ul/miage/Restaurant/Resto/Utilisateurs/Directeur.java
+++ src/main/java/fr/ul/miage/Restaurant/Resto/Utilisateurs/Directeur.java
@@ -63,7 +63,8 @@ public class Directeur extends Utilisateur {
 
                 break;
             case 4:
-
+                Connection conn = GestionBDD.connect();
+                getProfitDiner(conn);
                 break;
             case 5:
 
@@ -323,5 +324,45 @@ public class Directeur extends Utilisateur {
     public void créerCarteJour_Plat(Connection conn, Integer carte, Integer plat) {
         String sql = "INSERT INTO cartejour_plat(carte, plat)VALUES ('" + carte + "',' " + plat + "')";
         GestionBDD.executeUpdate(conn, sql);
+    }
+
+    /**
+     * Méthode pour obtenir le profit du déjeuner du jour courant
+     * @param conn la connection à la base
+     */
+    public void getProfitDejeuner (Connection conn){
+        SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        String sql = "SELECT sum(plat.prixplat) FROM commande JOIN souscommande ON souscommande.commande = commande.numerocommande JOIN plat ON plat.idplat = souscommande.plat WHERE commande.service = 'Déjeuner' AND commande.datecommande = '" + date + "' AND commande.statuscommande = 'Payée' GROUP BY commande.numerocommande";
+        ResultSet rs = GestionBDD.executeSelect(conn, sql);
+        try {
+            Long somme = Long.valueOf(0);
+            while (rs.next()){
+                somme += rs.getLong(1);
+            }
+            System.out.println("Le profit du déjeuner est de : " + somme + " €");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    /**
+     * Méthode pour obtenir le profit du diner du jour courant
+     * @param conn la connection à la base
+     */
+    public void getProfitDiner(Connection conn){
+        SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        String sql = "SELECT sum(plat.prixplat) FROM commande JOIN souscommande ON souscommande.commande = commande.numerocommande JOIN plat ON plat.idplat = souscommande.plat WHERE commande.service = 'Diner' AND commande.datecommande = '" + date + "' AND commande.statuscommande = 'Payée' GROUP BY commande.numerocommande";
+        ResultSet rs = GestionBDD.executeSelect(conn, sql);
+        try {
+            Long somme = Long.valueOf(0);
+            while (rs.next()){
+                somme += rs.getLong(1);
+            }
+            System.out.println("Le profit du diner est de : " + somme + " €");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
